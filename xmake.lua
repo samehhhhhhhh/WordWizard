191d73dcccdb56a6825d43435c1a8a9ea101a8c5
Samehh. <lepianistejunior@gmail.com>
2026-03-14 12:40:45 +0000

    Made the assets folder be automaticaly copied to the build folder

--- xmake.lua
+++ xmake.lua
@@ -7,6 +7,11 @@ target("WordWizard")
     add_includedirs("include")
     set_kind("binary")
 
+    after_build(function (target)
+        import("core.project.config")
+        os.cp("assets", path.join(target:targetdir(), "assets"))
+    end)
+    
     add_files("src/*.cpp")
     add_packages("sfml", "tmxlite")
 
